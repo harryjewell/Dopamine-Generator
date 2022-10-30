d75f5b96f6e532c9df7532057782ba2de9faad89
Harryjewell <harryjewell00@gmail.com>
2022-10-30 14:23:21 +1100

    FINAL COMMIT
    The final Github Commit for this assessment.
    
    Fixed up media queries to add responsiveness for smaller screen sizes, added comments to HTML file, formatted code, updated README file with production journal.

--- README.md
+++ README.md
@@ -1,12 +1,55 @@
-Production Journal - Harry Morton (Harry Jewell) u31566409
+Production Journal - Harry Morton (Harry Jewell) u3156409
 
-IDEATION
+HOW TO OPEN SITE WITH CODE ONLY
+Please download the code from Github and run from microsoft Visual Studio with ritwickdey.LiveServer extension.
+When extension is installed click the "Go Live" button in the blue section of the bottom right hand corner of Microsoft VS
+to see the site display properly. Otherwise, the fully functional site may be viewed on glitch.
 
-This project went through a large ideation cycle, while I usually arrive at a concept within a relatively short space of time,
+
+IDEATION ====================================================================================================================
+This project went through a long ideation cycle, while I usually arrive at a concept within a relatively short space of time,
 I decided to take my time on this project to produce something that was: 
 
-a) Realistic to produce within the production timeframe
-b) Within reach of my current coding and programming knowledge/skillset
-c) A personally interesting/unique concept to engage with
+a) Realistic to produce within the production timeframe.
+b) Within reach of my current coding and programming knowledge/skillset.
+c) A personally interesting/unique concept to engage with.
 
 
+PRODUCTION PROCESS ==========================================================================================================
+After deciding on the "Dopamine Generator" concept, I built out the basic site layout using an established "card" system, 
+utilizing simple HTML and CSS to determine the sites responsiveness and overall layout.
+
+The concept itself was drawn from prior knowledge of and research into existing API's that I believed would be relatively 
+simple to deploy to the site with my current skill level. With help from Ben I was able to get an "Advice" API bringing in
+new advice on each refresh of the site.
+
+With help from Ben and some experimentation I was able to introduce a dog API to the site, retrieving a new picture of a 
+dog on each refresh of the page. Further experiments allowed me to connect a "not so functional" spotify API after 
+following some tutorials on youtube, and generating a series of gifs from a selected search term on the site using the 
+Giphy API.
+
+In class Ben helped me with some simple solutions to easily embed a spotify player using spotify's API, and get the giphy
+API functioning. Taking the project home I worked on styling the existing elements and adding animations to the site.
+
+After a lot of struggling with some pretty mid-tier CSS I managed to get things looking pretty good regarding layout, 
+out of the realm of basic and into the realm of early 2000's web design aesthetic, the project came closer to looking
+like what I had pictured in my head day by day.
+
+Finally, on the approach to the final due date of the assignment, I recieved some help from Jarryd B (peer and fellow student).
+Jarryd helped with adding the remaining desired functionality, being getting a new picture of a dog on each click of the
+"new doggo" buttons, and looking into the possibility of targeting the "next song" button in the spotify widget with the
+"new music" button in the sites navbar.
+
+After a few hours ideation, testing and efforts the site was entirely functional, we added the "rave party" function, 
+dramatically increasing the sites "novel" capacity.
+After the code sesh I added comments to all of the code, deleted obselete code, and minimally altered some of the site styling.
+
+I've added some media queries with comments for responsive display on smaller screens to finalise the code and handle
+some display issues (header height, quote text size rendering new quote button non functional).
+Next I'll upload a final Github commit, post that commit to glitch to create a live site, and submit the final assessment 
+of my degree, what a milestone.
+
+Huge thanks to Ben and Jarryd for helping me through this one! If you've read through this, you're a real one.
+Cheers!
+
+Harry
--- main.js
+++ main.js
@@ -1,125 +1,124 @@
 //DOG CALL from https://docs.thedogapi.com/ ==========================================================================================================
-function getDoggo(){
+function getDoggo() {
   fetch('https://api.thedogapi.com/v1/images/search').then(response => response.json())
-  .then(response => {
+    .then(response => {
       console.log(response);
-  
+
       //api call that we want to show
       const doggo = response[0].url;
-  
+
       //select html on the page
       const select = document.getElementById('doggo');
       //update html with api data
       select.style.backgroundImage = "url('" + doggo + "')";
-  }
-      
-      )
-  .catch(err => console.error(err));
-  }
-    
-  
-  // RAVE PARTY FUNCTION ==========================================================================================================
-  function startRaveParty(){
-    document.getElementById("soundo").classList.add("raveParty");
-    document.getElementById("quoto").classList.add("raveParty");
-    document.getElementById("giffo").classList.add("raveParty");
-    document.getElementById("doggo").classList.add("raveParty");
-    document.getElementById("heado").classList.add("raveParty");
-    document.getElementById("navo").classList.add("raveParty");
-    document.getElementById("bodyo").classList.add("raveParty");
-  
-    // SCROLLS DOWN 500PX WHEN NEW MUSIC BUTTON CLICKED IN NAV
-    window.scrollBy(0, 500);
-  
-    // SETS TIMER FOR RAVE PARTY 8000 = 8 SECONDS
-    setTimeout(() => {
-      endRaveParty();
-    }, 8000);
-  }
-  
-  // ENDS RAVE PARTY FUNCTION AFTER TIMEOUT
-  function endRaveParty(){
-    document.getElementById("soundo").classList.remove("raveParty");
-    document.getElementById("quoto").classList.remove("raveParty");
-    document.getElementById("giffo").classList.remove("raveParty");
-    document.getElementById("doggo").classList.remove("raveParty");
-    document.getElementById("heado").classList.remove("raveParty");
-    document.getElementById("navo").classList.remove("raveParty");
-    document.getElementById("bodyo").classList.remove("raveParty");
-  }
-  
-  // NEW MUSIC BUTTON (NAV) ACTIVATES RAVE PARTY ON JUST SPOTIFY DIV - ACTIVATES HIDDEN TEXT ==========================================================
-  function startNewMusic(){
-      document.getElementById("newMusic").classList.remove("hidden");
-      document.getElementById("soundo").classList.add("raveParty");
-      window.scrollBy(0, 500);
-  
-// SETS 8 SECOND TIMEOUT FOR FUNCTION
-      setTimeout(() => {
-        endNewMusic();
-      }, 8000);
-  }
-  
-  // ENDS RAVE PARTY ON SPOTIFY DIV
-  function endNewMusic(){
-    document.getElementById("newMusic").classList.add("hidden");
-    document.getElementById("soundo").classList.remove("raveParty");
-  }
-  
-  // LOADS DOG AND QUOTE
-  function loading(){
-    getQuoto();
-    getDoggo();
-  }
-  
-  // QUOTE JS ========================================================================================================================================
-  function getQuoto(){
+    }
+
+    )
+    .catch(err => console.error(err));
+}
+
+
+// RAVE PARTY FUNCTION ==========================================================================================================
+function startRaveParty() {
+  document.getElementById("soundo").classList.add("raveParty");
+  document.getElementById("quoto").classList.add("raveParty");
+  document.getElementById("giffo").classList.add("raveParty");
+  document.getElementById("doggo").classList.add("raveParty");
+  document.getElementById("heado").classList.add("raveParty");
+  document.getElementById("navo").classList.add("raveParty");
+  document.getElementById("bodyo").classList.add("raveParty");
+
+  // SCROLLS DOWN 500PX WHEN RAVE PARTY BUTTON CLICKED IN NAV
+  // window.scrollBy(0, 500);
+
+  // SETS TIMER FOR RAVE PARTY 8000 = 8 SECONDS
+  setTimeout(() => {
+    endRaveParty();
+  }, 8000);
+}
+
+// ENDS RAVE PARTY FUNCTION AFTER TIMEOUT
+function endRaveParty() {
+  document.getElementById("soundo").classList.remove("raveParty");
+  document.getElementById("quoto").classList.remove("raveParty");
+  document.getElementById("giffo").classList.remove("raveParty");
+  document.getElementById("doggo").classList.remove("raveParty");
+  document.getElementById("heado").classList.remove("raveParty");
+  document.getElementById("navo").classList.remove("raveParty");
+  document.getElementById("bodyo").classList.remove("raveParty");
+}
+
+// NEW MUSIC BUTTON (NAV) ACTIVATES RAVE PARTY ON JUST SPOTIFY DIV - ACTIVATES HIDDEN TEXT ==========================================================
+function startNewMusic() {
+  document.getElementById("newMusic").classList.remove("hidden");
+  document.getElementById("soundo").classList.add("raveParty");
+  window.scrollBy(0, 500);
+
+  // SETS 8 SECOND TIMEOUT FOR FUNCTION
+  setTimeout(() => {
+    endNewMusic();
+  }, 8000);
+}
+
+// ENDS RAVE PARTY ON SPOTIFY DIV
+function endNewMusic() {
+  document.getElementById("newMusic").classList.add("hidden");
+  document.getElementById("soundo").classList.remove("raveParty");
+}
+
+// LOADS DOG AND QUOTE
+function loading() {
+  getQuoto();
+  getDoggo();
+}
+
+// QUOTE JS ========================================================================================================================================
+function getQuoto() {
   var quote = document.getElementById("quote");
   var author = document.getElementById("author");
   var btn = document.getElementById("btn");
-  
+
   const url = "https://api.quotable.io/random";
-  
+
   // let getQuote = () => {
-    fetch(url)
-      .then((data) => data.json())
-      .then((item) => {
-        quote.innerText = item.content;
-        author.innerText = item.author;
-      });
-  };
-  
-  //GIF JS =========================================================================================================================================
-  var api = 'https://api.giphy.com/v1/gifs/random?';
-  var apiKey = 'api_key=U96g4osMHY2nW7ZQrhg0YIsPCANziiTL';
-  // var query = '&q=cat+dog';
-  var query = '&tag=&rating=g';
-  
-  let gif = document.getElementById("gifContainer");
-  
-  https://api.giphy.com/v1/gifs/random?api_key=U96g4osMHY2nW7ZQrhg0YIsPCANziiTL&tag=&rating=g
-  
-  function setup() {
-    //noCanvas();
-    var url = api + apiKey + query;
-    console.log(url);
-    loadJSON(url, gotData);
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      quote.innerText = item.content;
+      author.innerText = item.author;
+    });
+};
+
+//GIF JS =========================================================================================================================================
+var api = 'https://api.giphy.com/v1/gifs/random?';
+var apiKey = 'api_key=U96g4osMHY2nW7ZQrhg0YIsPCANziiTL';
+// var query = '&q=cat+dog';
+var query = '&tag=&rating=g';
+
+let gif = document.getElementById("gifContainer");
+
+https://api.giphy.com/v1/gifs/random?api_key=U96g4osMHY2nW7ZQrhg0YIsPCANziiTL&tag=&rating=g
+
+function setup() {
+  //noCanvas();
+  var url = api + apiKey + query;
+  console.log(url);
+  loadJSON(url, gotData);
+}
+
+// RETRIEVES AND DISPLAYS GIF
+function gotData(giphy) {
+  console.log(giphy);
+  for (var i = 0; i < giphy.data.length; i++) {
+    createImg(giphy.data[i].images.original.url);
   }
-  
-  // RETRIEVES AND DISPLAYS RANDOM GIF
-  function gotData(giphy) {
-      console.log(giphy);
-    for (var i = 0; i < giphy.data.length; i++) {
-      createImg(giphy.data[i].images.original.url);
-    }
-    var image = giphy.data.images.fixed_height.url;
-    console.log(image);
-  
-    var gifStr = "<img src='" + image + "' alt='gif' >";
-  
-    console.log(gifStr);
-  
-    gif.innerHTML = gifStr
-    
-  }
-  
+  var image = giphy.data.images.fixed_height.url;
+  console.log(image);
+
+  var gifStr = "<img src='" + image + "' alt='gif' >";
+
+  console.log(gifStr);
+
+  gif.innerHTML = gifStr
+
+}
